a26e055f9b41015bdf03c80ab4b729b34773a332
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2025-01-03 13:08:55 +0000

    a

---
+++ dragform/app/Pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Canvas from '../components/Canvas';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>No-Code Platform</h1>
+      <Canvas />
+    </div>
+  );
+};
+
+export default Home;
