8ad0d49b357a04d25eeacbec769d91427c9de484
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2025-01-08 12:10:41 +0000

    a

--- dragform/app/components/Canvas.tsx
+++ dragform/app/components/Canvas.tsx
@@ -1,55 +1,20 @@
-'use client';
-
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { useDrop } from 'react-dnd';
-import DraggableItem from './DraggableItem';
 
 const Canvas: React.FC = () => {
-  const [items, setItems] = useState<any[]>([]);
-  const canvasRef = useRef<HTMLDivElement>(null);
-
-  const [, drop] = useDrop({
-    accept: 'BOX',
-    drop: (item: any, monitor) => {
-      const offset = monitor.getClientOffset();
-      const canvasBounds = canvasRef.current?.getBoundingClientRect();
-
-      if (offset && canvasBounds) {
-        const x = offset.x - canvasBounds.left;
-        const y = offset.y - canvasBounds.top;
-
-        setItems((prev) => [
-          ...prev,
-          {
-            id: item.id,
-            type: item.type,
-            position: { x, y },
-          },
-        ]);
-      }
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'component',
+    drop: (item, monitor) => {
+      console.log('Dropped item:', item);
     },
-  });
-
-  drop(canvasRef);
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
 
   return (
-    <div
-      ref={canvasRef}
-      style={{
-        width: '100%',
-        height: '80vh',
-        border: '1px solid black',
-        position: 'relative',
-      }}
-    >
-      {items.map((item, index) => (
-        <DraggableItem
-          key={index}
-          id={item.id}
-          type={item.type}
-          position={item.position}
-        />
-      ))}
+    <div ref={drop} className={`flex-grow p-4 ${isOver ? 'border-2 border-green-500' : 'border-2 border-black'}`}>
+      Drop components here
     </div>
   );
 };
